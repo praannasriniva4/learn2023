60cbcf1e3e7377adb5f28329a8e998eb3fda4601
prasanna <prasannasrinivas6875@gmail.com>
2023-06-16 10:42:22 +0530

    folder created

---
+++ module1/day1/l1/exp1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n1,n2, max;
+     scanf("%d %d",&n1,&n2);
+     max = (n1 > n2) ? n1 : n2;
+    printf("Largest number between %d and %d is %d. ",
+                                         n1, n2, max);
+ 
+    return 0;
+}
